db37fe5840031b621b469f0756c34257ae40e4fb
Minko Minkov <minccccc.mm@gmail.com>
2022-10-08 19:09:23 +0300

    Add some comments

--- contracts/DExchange.sol
+++ contracts/DExchange.sol
@@ -56,9 +56,10 @@ contract DExchange {
         _;
     }
 
-    modifier enoughBalance(address _address, uint _amount) {
-        uint balance = tokenBalances[msg.sender][_address];
-        require(balance >= _amount, "Insufficient balance");
+    /// @notice Validate caller has sufficient token balanace on the exchange
+    modifier enoughBalance(address tokenAddress, uint amount) {
+        uint balance = tokenBalances[msg.sender][tokenAddress];
+        require(balance >= amount, "Insufficient balance");
         _;
     }
 
@@ -70,6 +71,15 @@ contract DExchange {
         locked = false;
     }
 
+    /// @notice Validate input fields are positive numbers
+    modifier validOrderInput(uint price, uint tokenAmount) {
+        require(tokenAmount > 0, 'Order token amount can not be 0');
+        require(price > 0, 'Order price can not be 0');
+        _;
+    }
+
+    /// @dev Adds a new token address to the list of tokens on the exchange  
+    /// @param token Address of the token contract
     function addToken(ERC20 token) external {
         require(msg.sender == owner, "Only the owner can add tokens into the exchange");
         require(address(token) != address(0), "Token address is empty");
@@ -87,6 +97,7 @@ contract DExchange {
         sellOrders[address(token)] = new OrderManager(OrderType.ASC);
     }
 
+    /// @dev Returns all listed tokens ot the exchange
     function getListedTokens() external view returns (ListedToken[] memory) {
         ListedToken[] memory result = new ListedToken[](listedTokensArray.length);
         for (uint i = 0; i < listedTokensArray.length; i++) {
@@ -95,6 +106,8 @@ contract DExchange {
         return result;
     }
 
+    /// @dev Returns balance of all listed tokens (on exchange and on wallet) 
+    /// for current user
     function getMyBalance() external view returns (Balance[] memory) {
         Balance[] memory result = new Balance[](listedTokensArray.length);
         for (uint i = 0; i < listedTokensArray.length; i++) {
@@ -108,11 +121,18 @@ contract DExchange {
         return result;
     }
 
+    /// @dev Returns token balance for current user
+    /// @param tokenAddress Token address to check the balance
     function checkBalance(address tokenAddress) external view returns(uint) {
         return tokenBalances[msg.sender][tokenAddress];
     }
 
-    function deposit(address tokenAddress, uint tokenAmount) external noReentrancy tokenToBeListed(tokenAddress){
+    /// @dev Deposit tokens from users wallet
+    /// @notice To be able to call "transferFrom" the user have to accept this transfer in advance
+    /// @param tokenAddress Token address
+    /// @param tokenAmount Amount to be deposited
+    function deposit(address tokenAddress, uint tokenAmount) external 
+        noReentrancy tokenToBeListed(tokenAddress){
 
         tokenBalances[msg.sender][tokenAddress] += tokenAmount;       
         listedTokens[tokenAddress].tokenContract.transferFrom(msg.sender, address(this), tokenAmount);
@@ -120,6 +140,9 @@ contract DExchange {
         emit TokensDeposited(msg.sender, tokenAddress, tokenAmount);
     }
 
+    /// @dev Withdraw tokens to the users wallet
+    /// @param tokenAddress Token address
+    /// @param tokenAmount Amount to be withdrawn
     function withdraw(address tokenAddress, uint tokenAmount) external 
             noReentrancy tokenToBeListed(tokenAddress) enoughBalance(tokenAddress, tokenAmount) {
 
@@ -129,11 +152,12 @@ contract DExchange {
         emit Withdrawn(msg.sender, tokenAddress, tokenAmount);
     }
 
+    /// @dev Add buy order to the order list
+    /// @param tokenAddress Token to be bought
+    /// @param price Price for a token in ETH
+    /// @param tokenAmount Amount of tokens to be bought
     function placeBuyOrder(address tokenAddress, uint price, uint tokenAmount) external payable
-            tokenToBeListed(tokenAddress) {
-
-        require(tokenAmount > 0, 'Order token amount can not be 0');
-        require(price > 0, 'Order price can not be 0');
+            tokenToBeListed(tokenAddress) validOrderInput(price, tokenAmount) {
 
         uint eth = 1 ether;
         uint _orderPrice = tokenAmount.mul(price).div(eth);
@@ -154,6 +178,9 @@ contract DExchange {
         emit BuyOrderPlaced(tokenAddress, price, tokenAmount);
     }
 
+    /// @dev Remove buy order
+    /// @param tokenAddress Order token address
+    /// @param orderId Id ot the order
     function cancelBuyOrder(address tokenAddress, uint orderId) external tokenToBeListed(tokenAddress) {
         Order memory order = buyOrders[tokenAddress].cancelOrder(orderId);
                 
@@ -164,18 +191,22 @@ contract DExchange {
         emit OrderCanceled(tokenAddress, orderId);
     }
 
+    /// @dev Add sell order to the order list
+    /// @param tokenAddress Address ot the token to be sold
+    /// @param price Price for a token in ETH
+    /// @param tokenAmount Amount of tokens to be sold
     function placeSellOrder(address tokenAddress, uint price, uint tokenAmount) external
-            tokenToBeListed(tokenAddress) enoughBalance(tokenAddress, tokenAmount) {
+            tokenToBeListed(tokenAddress) enoughBalance(tokenAddress, tokenAmount) validOrderInput(price, tokenAmount) {
         
-        require(tokenAmount > 0, 'Order token amount can not be 0');
-        require(price > 0, 'Order price can not be 0');
-
         sellOrders[tokenAddress].addOrder(msg.sender, price, tokenAmount);
         tokenBalances[msg.sender][tokenAddress] = tokenBalances[msg.sender][tokenAddress].sub(tokenAmount);
         
         emit SellOrderPlaced(tokenAddress, price, tokenAmount);
     }
 
+    /// @dev Remove sell order
+    /// @param tokenAddress Order token address
+    /// @param orderId Id ot the order
     function cancelSellOrder(address tokenAddress, uint orderId) external tokenToBeListed(tokenAddress) {
         Order memory order = sellOrders[tokenAddress].cancelOrder(orderId);
         tokenBalances[order.user][tokenAddress] = tokenBalances[order.user][tokenAddress].add(order.amount);
@@ -183,38 +214,58 @@ contract DExchange {
         emit OrderCanceled(tokenAddress, orderId);
     }
 
+    /// @dev Returns sorted DisplayOrder list of the top/first buy orders to be executed
+    /// @param tokenAddress Specifies token address
     function getTopBuyOrders(address tokenAddress) external view 
         tokenToBeListed(tokenAddress) returns(DisplayOrder[] memory) {
 
         return buyOrders[tokenAddress].getTopOrders(maxDispayOrders);
     }
 
+    /// @dev Returns sorted DisplayOrder list of the top/first sell orders to be executed
+    /// @param tokenAddress Specifies token address
     function getTopSellOrders(address tokenAddress) external view 
         tokenToBeListed(tokenAddress) returns(DisplayOrder[] memory) {
         
         return sellOrders[tokenAddress].getTopOrders(maxDispayOrders);
     }
 
+    /// @dev Returns sorted DisplayOrder list with sell orders from the caller
+    /// @param tokenAddress Specifies token address
     function getMyBuyOrders(address tokenAddress) external view
         tokenToBeListed(tokenAddress) returns(DisplayOrder[] memory) {
         return buyOrders[tokenAddress].getTopOrders(maxDispayOrders);
     }
 
+    /// @dev Returns sorted DisplayOrder list with sell orders from the caller
+    /// @param tokenAddress Specifies token address
     function getMySellOrders(address tokenAddress) external view
         tokenToBeListed(tokenAddress) returns(DisplayOrder[] memory) {
         return sellOrders[tokenAddress].getTopOrders(maxDispayOrders);
     }
 
+    /// @dev Calculate how many tokens the user can buy
+    /// @param tokenAddress Token address
+    /// @param purchasePrice The amount in ETH for the purchase
+    /// @return Amount of tokens the user can buy at the moment for the specified amount ot ETH
     function calculateBuyTokensAmount(address tokenAddress, uint purchasePrice) external view
         tokenToBeListed(tokenAddress) returns(uint) {
         return sellOrders[tokenAddress].calculatePurchaseTokensAmount(purchasePrice);
     }
 
+    /// @dev Calculate how many tokens the user can sell
+    /// @param tokenAddress Token address
+    /// @param purchaseAmount Amount of tokens to be sell
+    /// @return Amount of ETHs the user can get at the moment if sell the tokens
     function calculateSellTokensPrice(address tokenAddress, uint purchaseAmount) external view
         tokenToBeListed(tokenAddress) returns(uint) {
         return buyOrders[tokenAddress].calculatePurchaseTokensAmount(purchaseAmount);
     }
 
+    /// @notice Buy tokens (market buy) and if any charge send it back to the user
+    ///         Will send the ETHs for each executed order to the resposible seller.
+    /// @dev The amount of tokens will be calculated based on ETH send to the function
+    /// @param tokenAddress Token address
     function buyTokens(address tokenAddress) external payable
         noReentrancy tokenToBeListed(tokenAddress) {
         
@@ -241,11 +292,14 @@ contract DExchange {
         emit TokensPurchased(msg.sender, tokenAddress, msg.value - charge, amount);
     }
 
+    /// @notice Sell tokens (market sell), the amount of ETH from the sale will be send to the user
+    ///         address and sold tokens will be splitter between executed buy orders (on the exchange)
+    /// @param tokenAddress Token address
+    /// @param tokenAddress Amount of tokens to be sold
     function sellTokens(address tokenAddress, uint tokenAmount) external 
-        noReentrancy tokenToBeListed(tokenAddress) {
+        noReentrancy enoughBalance(tokenAddress, tokenAmount) tokenToBeListed(tokenAddress) {
         
         require(tokenAmount > 0, "Can not sell 0 tokens");
-        require(tokenBalances[msg.sender][tokenAddress] >= tokenAmount, "You don't have enough tokens");
         (Order[] memory executedOrders, uint amount,) = buyOrders[tokenAddress].processOrder(tokenAmount);
 
         require(executedOrders.length > 0, "There is no executed order");
